86dee1fbf22089b3d87cdeebfb31c61be1e93edf
Andrea Leopardi <an.leopardi@gmail.com>
2015-08-11 15:30:02 +0200

    Take advantage of Recs.URI and add some tests

--- lib/recs.ex
+++ lib/recs.ex
@@ -9,10 +9,16 @@ defmodule Recs do
 
   @redis_opts ~w(host port password database)a
 
-  @spec start_link(Keyword.t) :: GenServer.on_start
-  def start_link(opts \\ []) do
+  @spec start_link(Keyword.t | binary) :: GenServer.on_start
+  def start_link(uri_or_opts \\ [])
+
+  def start_link(uri) when is_binary(uri) do
+    uri |> Recs.URI.opts_from_uri |> start_link
+  end
+
+  def start_link(opts) do
     {_redis_opts, connection_opts} = Keyword.split(opts, @redis_opts)
-    opts = Keyword.merge(@default_opts, opts)
+    opts = merge_with_default_opts(opts)
     Connection.start_link(Recs.Connection, opts, connection_opts)
   end
 
@@ -24,5 +30,14 @@ defmodule Recs do
   @spec pipeline(pid, [command]) :: [Recs.Protocol.redis_value]
   def pipeline(conn, commands) do
     Connection.call(conn, {:pipeline, commands})
+  end
+
+  defp merge_with_default_opts(opts) do
+    Keyword.merge @default_opts, opts, fn
+      _k, default_val, nil ->
+        default_val
+      _k, _default_val, val ->
+        val
+    end
   end
 end
--- test/recs_test.exs
+++ test/recs_test.exs
@@ -2,6 +2,12 @@ defmodule RecsTest do
   use ExUnit.Case, async: true
   import Recs.TestHelpers
 
+  setup_all do
+    {:ok, conn} = Recs.start_link
+    Recs.command(conn, ["FLUSHDB"])
+    {:ok, %{}}
+  end
+
   setup context do
     if context[:no_setup] do
       {:ok, %{}}
@@ -43,9 +49,14 @@ defmodule RecsTest do
     end
   end
 
+  @tag :no_setup
+  test "start_link/1: using a redis:// url" do
+    assert {:ok, pid} = Recs.start_link "redis://localhost:6379/3"
+    assert Recs.command(pid, ["PING"]) == "PONG"
+  end
+
   test "command/2", %{conn: c} do
-    assert Recs.command(c, ["SET", "foo", "1"]) == "OK"
-    assert Recs.command(c, ["GET", "foo"]) == "1"
+    assert Recs.command(c, ["PING"]) == "PONG"
   end
 
   test "pipeline/2", %{conn: c} do
@@ -55,5 +66,84 @@ defmodule RecsTest do
       ["GET", "pipe"],
     ]
     assert Recs.pipeline(c, commands) == ["OK", 11, "11"]
+  end
+
+  test "some commands: APPEND", %{conn: c} do
+    assert Recs.command(c, ~w(APPEND to_append hello)) == 5
+    assert Recs.command(c, ~w(APPEND to_append world)) == 10
+  end
+
+  test "some commands: DBSIZE", %{conn: c} do
+    i = Recs.command(c, ["DBSIZE"])
+    assert is_integer(i)
+  end
+
+  test "some commands: INCR and DECR", %{conn: c} do
+    assert Recs.command(c, ["INCR", "to_incr"]) == 1
+    assert Recs.command(c, ["DECR", "to_incr"]) == 0
+  end
+
+  test "some commands: transactions with MULTI/EXEC (executing)", %{conn: c} do
+    assert Recs.command(c, ["MULTI"]) == "OK"
+
+    assert Recs.command(c, ["INCR", "multifoo"]) == "QUEUED"
+    assert Recs.command(c, ["INCR", "multibar"]) == "QUEUED"
+    assert Recs.command(c, ["INCRBY", "multifoo", 4]) == "QUEUED"
+
+    assert Recs.command(c, ["EXEC"]) == [1, 1, 5]
+  end
+
+  test "some commands: transactions with MULTI/DISCARD", %{conn: c} do
+    "OK" = Recs.command(c, ["SET", "discarding", "foo"])
+
+    assert Recs.command(c, ["MULTI"]) == "OK"
+    assert Recs.command(c, ["SET", "discarding", "bar"]) == "QUEUED"
+
+    # Discarding
+    assert Recs.command(c, ["DISCARD"]) == "OK"
+    assert Recs.command(c, ["GET", "discarding"]) == "foo"
+  end
+
+  test "some commands: TYPE", %{conn: c} do
+    assert Recs.command(c, ["SET", "string_type", "foo bar"]) == "OK"
+    assert Recs.command(c, ["TYPE", "string_type"]) == "string"
+  end
+
+  test "some commands: STRLEN", %{conn: c} do
+    assert Recs.command(c, ["SET", "string_length", "foo bar"]) == "OK"
+    assert Recs.command(c, ["STRLEN", "string_length"]) == 7
+  end
+
+  test "some commands: LPUSH, LLEN, LPOP, LINDEX", %{conn: c} do
+    assert Recs.command(c, ~w(LPUSH mylist world)) == 1
+    assert Recs.command(c, ~w(LPUSH mylist hello)) == 2
+    assert Recs.command(c, ~w(LLEN mylist)) == 2
+    assert Recs.command(c, ~w(LINDEX mylist 0)) == "hello"
+    assert Recs.command(c, ~w(LPOP mylist)) == "hello"
+  end
+
+  test "Lua scripting: EVAL", %{conn: c} do
+    script = """
+    redis.call("SET", "evalling", "yes")
+    return {KEYS[1],ARGV[1],ARGV[2]}
+    """
+
+    cmds = ["eval", script, "1", "key", "first", "second"]
+
+    assert Recs.command(c, cmds) == ["key", "first", "second"]
+    assert Recs.command(c, ["GET", "evalling"]) == "yes"
+  end
+
+  test "Lua scripting: SCRIPT LOAD, SCRIPT EXISTS, EVALSHA", %{conn: c} do
+    script = """
+    return 'hello world'
+    """
+
+    sha = Recs.command(c, ["SCRIPT", "LOAD", script])
+    assert is_binary(sha)
+    assert Recs.command(c, ["SCRIPT", "EXISTS", sha, "foo"]) == [1, 0]
+
+    # Eval'ing the script
+    assert Recs.command(c, ["EVALSHA", sha, 0]) == "hello world"
   end
 end
