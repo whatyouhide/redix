d5fa01013cfcafae848746bf9fcd6388a13d01c0
Andrea Leopardi <an.leopardi@gmail.com>
2015-08-11 15:29:52 +0200

    Add Recs.URI to parse connection uris

---
+++ lib/recs/uri.ex
@@ -0,0 +1,36 @@
+defmodule Recs.URI do
+  defmodule URIError do
+    defexception [:message]
+  end
+
+  def opts_from_uri(uri) when is_binary(uri) do
+    uri = URI.parse(uri)
+    ensure_scheme_is_redis!(uri)
+    %URI{host: host, port: port} = uri
+
+    [
+      host: host,
+      port: port,
+      password: password(uri),
+      database: db(uri),
+    ]
+  end
+
+  defp ensure_scheme_is_redis!(%URI{scheme: "redis"}),
+    do: :ok
+  defp ensure_scheme_is_redis!(%URI{scheme: scheme}),
+    do: raise(URIError, message: "scheme is not redis:// but '#{scheme}://'")
+
+  defp password(%URI{userinfo: nil}) do
+    nil
+  end
+
+  defp password(%URI{userinfo: userinfo}) do
+    [_user, password] = String.split(userinfo, ":", parts: 2)
+    password
+  end
+
+  defp db(%URI{path: nil}), do: nil
+  defp db(%URI{path: "/"}), do: nil
+  defp db(%URI{path: "/" <> db}), do: String.to_integer(db)
+end
---
+++ test/recs/uri_test.exs
@@ -0,0 +1,50 @@
+defmodule Recs.URITest do
+  use ExUnit.Case, async: true
+
+  import Recs.URI
+  alias Recs.URI.URIError
+
+  test "opts_from_uri/1: invalid scheme" do
+    msg = "scheme is not redis:// but 'foo://'"
+    assert_raise URIError, msg, fn ->
+      opts_from_uri("foo://example.com")
+    end
+  end
+
+  test "opts_from_uri/1: just the host" do
+    opts = opts_from_uri("redis://example.com")
+    assert opts[:host] == "example.com"
+    assert is_nil(opts[:port])
+    assert is_nil(opts[:database])
+    assert is_nil(opts[:password])
+  end
+
+  test "opts_from_uri/1: host and port" do
+    opts = opts_from_uri("redis://localhost:6379")
+    assert opts[:host] == "localhost"
+    assert opts[:port] == 6379
+    assert is_nil(opts[:database])
+    assert is_nil(opts[:password])
+  end
+
+  test "opts_from_uri/1: password" do
+    opts = opts_from_uri("redis://:pass@localhost")
+    assert opts[:host] == "localhost"
+    assert opts[:password] == "pass"
+
+    # We don't care about the user.
+    opts = opts_from_uri("redis://user:pass@localhost")
+    assert opts[:host] == "localhost"
+    assert opts[:password] == "pass"
+  end
+
+  test "opts_from_uri/1: database" do
+    opts = opts_from_uri("redis://localhost/2")
+    assert opts[:host] == "localhost"
+    assert opts[:database] == 2
+
+    opts = opts_from_uri("redis://localhost/")
+    assert opts[:host] == "localhost"
+    assert is_nil(opts[:database])
+  end
+end
