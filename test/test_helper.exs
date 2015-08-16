4d3f76c3c85874c7d611e33fa989e09562e2f7ee
Andrea Leopardi <an.leopardi@gmail.com>
2015-08-16 23:32:17 +0200

    Ensure Redis is reachable in test_helper.exs
    I'm just connecting to Redis with `:gen_tcp.connect/3` to check if it's
    up: if it's not, I print a nice error message in the console and exit
    right away.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,14 @@
 ExUnit.start()
 
+case :gen_tcp.connect('localhost', 6379, []) do
+  {:ok, socket} ->
+    :gen_tcp.close(socket)
+  {:error, reason} ->
+    msg = "Cannot connect to Redis (http://localhost:6379): #{:inet.format_error(reason)}"
+    IO.puts :stderr, IO.ANSI.format([:red, msg])
+    System.halt(1)
+end
+
 defmodule Redix.TestHelpers do
   def silence_log(fun) do
     Logger.remove_backend :console
