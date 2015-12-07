5ae310e862eb0abc7e6b168b92b30cf30bc19b35
Andrea Leopardi <an.leopardi@gmail.com>
2015-12-07 10:11:42 +0100

    Reindent lists in mix.exs to follow conventions

--- mix.exs
+++ mix.exs
@@ -10,26 +10,24 @@ defmodule Redix.Mixfile do
   @version "0.3.0"
 
   def project do
-    [
-      app: :redix,
-      version: @version,
-      elixir: "~> 1.0",
-      build_embedded: Mix.env in [:prod, :bench],
-      start_permanent: Mix.env == :prod,
-      test_coverage: [tool: Coverex.Task],
-      deps: deps,
+    [app: :redix,
+     version: @version,
+     elixir: "~> 1.0",
+     build_embedded: Mix.env in [:prod, :bench],
+     start_permanent: Mix.env == :prod,
+     test_coverage: [tool: Coverex.Task],
+     deps: deps,
 
-      # Hex
-      package: package,
-      description: @description,
+     # Hex
+     package: package,
+     description: @description,
 
-      # Docs
-      name: "Redix",
-      docs: [main: "Redix",
-             source_ref: "v#{@version}",
-             source_url: @repo_url,
-             extras: ["README.md"]],
-    ]
+     # Docs
+     name: "Redix",
+     docs: [main: "Redix",
+            source_ref: "v#{@version}",
+            source_url: @repo_url,
+            extras: ["README.md"]]]
   end
 
   def application do
@@ -37,23 +35,19 @@ defmodule Redix.Mixfile do
   end
 
   defp package do
-    [
-      maintainers: ["Andrea Leopardi"],
-      licenses: ["MIT"],
-      links: %{"GitHub" => @repo_url},
-    ]
+    [maintainers: ["Andrea Leopardi"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => @repo_url}]
   end
 
   defp deps do
-    [
-      {:connection, "~> 1.0.0"},
-      {:dialyze, "~> 0.2", only: :dev},
-      {:benchfella, github: "alco/benchfella", only: :bench},
-      {:redo, github: "heroku/redo", only: :bench},
-      {:eredis, github: "wooga/eredis", only: :bench},
-      {:yar, github: "dantswain/yar", only: :bench},
-      {:coverex, "~> 1.4", only: :test},
-      {:ex_doc, ">= 0.0.0", only: :docs},
-    ]
+    [{:connection, "~> 1.0.0"},
+     {:dialyze, "~> 0.2", only: :dev},
+     {:benchfella, github: "alco/benchfella", only: :bench},
+     {:redo, github: "heroku/redo", only: :bench},
+     {:eredis, github: "wooga/eredis", only: :bench},
+     {:yar, github: "dantswain/yar", only: :bench},
+     {:coverex, "~> 1.4", only: :test},
+     {:ex_doc, ">= 0.0.0", only: :docs}]
   end
 end
