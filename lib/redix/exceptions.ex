dc03f9a3afb122e5cb39223459607b950b8222e6
Andrea Leopardi <an.leopardi@gmail.com>
2017-02-14 18:22:01 +0100

    Add docs to Redix.Error and Redix.ConnectionError

--- lib/redix/exceptions.ex
+++ lib/redix/exceptions.ex
@@ -1,6 +1,10 @@
 defmodule Redix.Error do
   @moduledoc """
   Error returned by Redis.
+
+  This exception represents semantic errors returned by Redis: for example,
+  non-existing commands or operations on keys with the wrong type (`INCR
+  not_an_integer`).
   """
 
   defexception [:message]
@@ -11,6 +15,31 @@ end
 defmodule Redix.ConnectionError do
   @moduledoc """
   Error in the connection to Redis.
+
+  This exception represents errors in the connection to Redis: for example,
+  request timeouts, disconnections, and similar.
+
+  ## Exception fields
+
+  This exception has the following public fields:
+
+    * `:reason` - (atom) the error reason. It can be one of the Redix-specific
+      reasons described in the "Error reasons" section below, or any error
+      reason returned by functions in the `:gen_tcp` module (see the
+      [`:inet.posix/0](http://www.erlang.org/doc/man/inet.html#type-posix) type.
+
+  ## Error reasons
+
+  The `:reason` field can assume a few Redix-specific values:
+
+    * `:closed`: when the connection to Redis is closed (and Redix is
+      reconnecting) and the user attempts to talk to Redis
+
+    * `:disconnected`: when the connection drops while a request to Redis is in
+      flight.
+
+    * `:timeout`: when Redis doesn't reply to the request in time.
+
   """
 
   defexception [:reason]
