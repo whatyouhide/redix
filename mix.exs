4a0627b118ea3087c103bfb47058533255dbe2d5
Andrea Leopardi <an.leopardi@gmail.com>
2017-08-02 11:51:48 +0100

    Improve formatting of lists in mix.exs
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -8,23 +8,27 @@ defmodule Redix.Mixfile do
   @version "0.6.1"
 
   def project() do
-    [app: :redix,
-     version: @version,
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
+    [
+      app: :redix,
+      version: @version,
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
 
-     # Hex
-     package: package(),
-     description: @description,
+      # Hex
+      package: package(),
+      description: @description,
 
-     # Docs
-     name: "Redix",
-     docs: [main: "Redix",
-            source_ref: "v#{@version}",
-            source_url: @repo_url,
-            extras: ["README.md", "pages/Reconnections.md", "pages/Real-world usage.md"]]]
+      # Docs
+      name: "Redix",
+      docs: [
+        main: "Redix",
+        source_ref: "v#{@version}",
+        source_url: @repo_url,
+        extras: ["README.md", "pages/Reconnections.md", "pages/Real-world usage.md"],
+      ],
+    ]
   end
 
   def application() do
@@ -32,13 +36,17 @@ defmodule Redix.Mixfile do
   end
 
   defp package() do
-    [maintainers: ["Andrea Leopardi", "Aleksei Magusev"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => @repo_url}]
+    [
+      maintainers: ["Andrea Leopardi", "Aleksei Magusev"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url},
+    ]
   end
 
   defp deps() do
-    [{:connection, "~> 1.0"},
-     {:ex_doc, "~> 0.15", only: :dev}]
+    [
+      {:connection, "~> 1.0"},
+      {:ex_doc, "~> 0.15", only: :dev},
+    ]
   end
 end
