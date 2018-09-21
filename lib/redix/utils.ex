79d4c58e6fb42bf79a47b7152cc843f40acdc453
Thomas Schmitt <thomas.schmitt@ni.com>
2018-09-21 03:33:51 -0700

    Fix badmatch when setting up the SSL buffers
    :ssl.getopts does not always return the options in the same order
    as requested resulting in :badmatch.

--- lib/redix/utils.ex
+++ lib/redix/utils.ex
@@ -120,7 +120,9 @@ defmodule Redix.Utils do
     inet_mod = if transport == :ssl, do: :ssl, else: :inet
 
     with {:ok, opts} <- inet_mod.getopts(socket, [:sndbuf, :recbuf, :buffer]) do
-      [sndbuf: sndbuf, recbuf: recbuf, buffer: buffer] = opts
+      sndbuf = Keyword.get(opts, :sndbuf)
+      recbuf = Keyword.get(opts, :recbuf)
+      buffer = Keyword.get(opts, :buffer)
       inet_mod.setopts(socket, buffer: buffer |> max(sndbuf) |> max(recbuf))
     end
   end
