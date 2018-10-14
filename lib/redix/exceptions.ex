ca567fda37ae9cd360eb8290edb9fc72b1987e41
Andrea Leopardi <an.leopardi@gmail.com>
2018-10-15 00:08:08 +0200

    Make Redix.ConnectionError.format_reason/1 private

--- lib/redix/exceptions.ex
+++ lib/redix/exceptions.ex
@@ -48,22 +48,18 @@ defmodule Redix.ConnectionError do
     format_reason(reason)
   end
 
-  @doc false
-  @spec format_reason(term) :: binary
-  def format_reason(reason)
-
   # :inet.format_error/1 doesn't format :tcp_closed.
-  def format_reason(:tcp_closed) do
+  defp format_reason(:tcp_closed) do
     "TCP connection closed"
   end
 
   # Manually returned by us when the connection is closed and someone tries to
   # send a command to Redis.
-  def format_reason(:closed) do
+  defp format_reason(:closed) do
     "the connection to Redis is closed"
   end
 
-  def format_reason(reason) do
+  defp format_reason(reason) do
     case :inet.format_error(reason) do
       'unknown POSIX error' ->
         inspect(reason)
