1b7a2ee32c4a03e14883594e16c35c1fe5d508da
Andrea Leopardi <an.leopardi@gmail.com>
2021-05-16 18:14:14 +0200

    WIP

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,10 +2,10 @@ ExUnit.start(assert_receive_timeout: 500)
 
 Logger.configure(level: :debug)
 
-case :gen_tcp.connect('localhost', 6379, []) do
-  {:ok, socket} ->
-    :ok = :gen_tcp.close(socket)
+# case :gen_tcp.connect('localhost', 6379, []) do
+#   {:ok, socket} ->
+#     :ok = :gen_tcp.close(socket)
 
-  {:error, reason} ->
-    Mix.raise("Cannot connect to Redis (http://localhost:6379): #{:inet.format_error(reason)}")
-end
+#   {:error, reason} ->
+#     Mix.raise("Cannot connect to Redis (http://localhost:6379): #{:inet.format_error(reason)}")
+# end
