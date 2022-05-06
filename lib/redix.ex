73fd32f5d72152752f07e53c8f5035490c6f7cd4
Alex Martsinovich <alex@slab.com>
2022-05-06 14:15:50 -0700

    Add warning for servers potentially blocking CLIENT commands

--- lib/redix.ex
+++ lib/redix.ex
@@ -73,6 +73,10 @@ defmodule Redix do
   Skipping replies is useful to improve performance when you want to issue many commands
   but are not interested in the responses to those commands.
 
+  > ⚠️ Some servers may block `CLIENT` commands.
+  > For example, Google Memorystorage is [known to do so](https://cloud.google.com/memorystore/docs/redis/product-constraints).
+  > If this is the case, functions mentioned above won't work.
+
   ## SSL
 
   Redix supports SSL by passing `ssl: true` in `start_link/1`. You can use the `:socket_opts`
